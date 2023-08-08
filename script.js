0c2623f8cdbe90a696fb5cc708738c29ac636ec9
CelesNach <128744015+CelesNach@users.noreply.github.com>
2023-08-08 17:46:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const nombre = prompt("¿Como es tu nombre?")
+document.querySelector("h1").innerHTML = nombre
+
+const edad = prompt("¿Cuantos años tienes?")
+document.querySelector("h2").innerHTML = edad
+
+console.log(nombre)
+console.log(edad)
+
+
